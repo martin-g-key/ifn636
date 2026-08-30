7bdb5676b88563bf17e47c4d515ce8834c324939
martin-g-key <contact.m.keylock@gmail.com>
2026-08-30 05:02:23 +0000

    full skeleton with auto testing and CI

--- frontend/test/UserList.test.js
+++ frontend/test/UserList.test.js
@@ -4,26 +4,34 @@
 const React = require('react');
 const { expect } = require('chai');
 const { renderToString } = require('react-dom/server');
-const userList = require('../src/components/userList')
+//const userList = require('../src/components/userList').default;
+
+
+const userList = require('../src/components/userList').default;
+console.log('Component is:', typeof userList, userList);
 
 // testing
 describe('userList component', () => {
   it('shows a friendly message when there are no users', () => {
     
-    const html = renderToString(React.createElement(userList, { username: [] }));
+
+
+
+    const html = renderToString(React.createElement(userList, { users: [] }));
     
     expect(html).to.contain('No users yet');
   });
 
   it('renders eacth username passed via props', () => {
     
-    const usernames = [
+    const users = [
         {id: 1, username: 'Vida.Lahey' },
         {id: 2, username: 'Jean.Broome-Norton' },
     ];
 
+
     const html = renderToString(React.createElement(userList, {
-        usernames }));
+        users }));
 
         expect(html).to.contain('Vida.Lahey')
         expect(html).to.contain('Jean.Broome-Norton')
